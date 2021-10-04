48f42fb98ef2e589dec74a3c495aa6d75999e01d
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-10-04 16:39:19 +0000

    adding container

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,11 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue[700],
       ),
-      body: Center(
-          child: IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.home),
-        iconSize: 60.0,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
         color: Colors.blue,
-      )),
+        child: const Text('Hello container'),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Text('Click'),
